41487e234cb867d33f0915007b9752a2f4056f2a
pixelzery <pixelzery@gmail.com>
2021-05-22 02:13:00 +0100

    improve Segment model + PaddleOCR segmenter

--- textq/model.py
+++ textq/model.py
@@ -1,11 +1,14 @@
 from typing import Optional, Iterable, Tuple
 
 class Segment:
-    def __init__(self, polygon: Iterable[Tuple], text: Optional[str] = None, confidence: Optional[float] = None):
+    def __init__(self, polygon: Iterable[Tuple], text: Optional[str] = None, confidence: Optional[float] = 1.0):
         self.polygon = polygon
         self.text = text
         self.confidence = confidence
 
     def __repr__(self):
-        return "[" + ", ".join(("(" + ", ".join(map(str, vertex)) + ")" for vertex in self.polygon)) + \
-               (" - {}".format(self.text) if self.text is not None else "") + "]"
+        return "<" + \
+               "[" + ", ".join(("(" + ", ".join(map(str, vertex)) + ")" for vertex in self.polygon)) + "]" + \
+               ', "{}" '.format(self.text) + \
+               ", {:.02f}".format(self.confidence) + \
+               ">"
--- textq/segmenters/paddleocr.py
+++ textq/segmenters/paddleocr.py
@@ -6,16 +6,19 @@ import cv2
 import numpy as np
 
 class PaddleOCRSegmenter(BaseSegmenter):
-    def __init__(self, paddle_ocr=None):
+    def __init__(self, paddle_ocr=None, rec=True):
         if paddle_ocr is None:
             paddle_ocr = PaddleOCR(use_angle_cls=False, lang='en')
 
         self.__ocr = paddle_ocr
+        self.rec = rec
 
     def segment(self, im) -> Iterable[model.Segment]:
         opencv_im = cv2.cvtColor(np.array(im), cv2.COLOR_RGB2BGR)
-        result = self.__ocr.ocr(opencv_im, cls=False, rec=False)
+        result = self.__ocr.ocr(opencv_im, cls=False, rec=self.rec)
 
         for line in result:
-            print(line)
-            yield model.Segment(line)
+            if self.rec:
+                yield model.Segment(line[0], *line[1])
+            else:
+                yield model.Segment(line[0])
