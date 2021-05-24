29632cefdbddaa9e71ae62546866720dde06b067
pixelzery <pixelzery@gmail.com>
2021-05-24 01:39:33 +0100

    reimplement caching system use one file only also, the previous method would collide under certain circumstances (which involved deleting files from disk)

--- textq/queriers.py
+++ textq/queriers.py
@@ -38,45 +38,26 @@ class TextQuerier(Hashable):
         if self.image is None:
             return
 
-        # load index
-        # NB: using index because some systems have restrictions on file name length
-        index = {}
-        index_path = os.path.join(self._cache_dir, "index.json")
-        if os.path.isfile(index_path):
-            with open(index_path, "r") as f:
-                index.update(json.load(f))
+        # load cache
+        cache = {}
+        cache_path = os.path.join(self._cache_dir, "cache.json")
+        if os.path.isfile(cache_path):
+            with open(cache_path, "r") as f:
+                cache.update(json.load(f))
 
         cur_hash = self.compute_hash()
 
         # return from cache if available
-        if cur_hash in index:
-            cache_path = os.path.join(self._cache_dir, os.path.normpath(index[cur_hash]))
-
-            if os.path.isfile(cache_path):
-                with open(cache_path, "r") as f:
-                    return [Region(**kw) for kw in json.load(f)]
-            else:
-                index.pop(cur_hash)
+        if cur_hash in cache:
+            return [Region(**kw) for kw in cache[cur_hash]]
 
         # was not in cache, so compute regions using engine
         regions = tuple(self.engine.run(self.image))
 
-        def int_to_string(num):
-            alphabet = "0123456789abcdefghijklmnopqrstuvwxyz"
-            if num == 0:
-                return ""
-            else:
-                return int_to_string(num // len(alphabet)) + alphabet[num % len(alphabet)]
-
-        # save regions to cache
-        rel_cache_path = int_to_string(len(os.listdir(self._cache_dir)) + 1) + ".json"
-        with open(os.path.join(self._cache_dir, rel_cache_path), "w") as f:
-            json.dump([region.__dict__ for region in regions], f)
-
-        # update and save index
-        index[cur_hash] = rel_cache_path
-        with open(index_path, "w") as f:
-            json.dump(index, f)
+        # update and save cache
+        cache[cur_hash] = [region.__dict__ for region in regions]
+        with open(cache_path, "w") as f:
+            json.dump(cache, f)
 
         return regions
 
